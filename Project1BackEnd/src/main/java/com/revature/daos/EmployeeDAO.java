4138786bd00b54688473d0c2b8a90d219ef31cbd
Tyler Throop <tyler.throop@gmail.com>
2021-06-14 08:40:12 -0700

    Adding approve request I think the logic will work but the business side of things is rather complex

--- Project1BackEnd/src/main/java/com/revature/daos/EmployeeDAO.java
+++ Project1BackEnd/src/main/java/com/revature/daos/EmployeeDAO.java
@@ -34,15 +34,51 @@ public class EmployeeDAO {
 		return null;
 	}
 	//request type is based on the different types.
-	public void createRequest(String request,int userid,String requestType) {
-		String sql = "insert into requests values(default,?,false,?,?);";
+	public boolean createRequest(String request,int userid,String requestType) {
+		String sql = "insert into requests values(default,?,false,?,?,'none','empty');";
+		
 		try {
 			PreparedStatement ps = conn.prepareStatement(sql);
 			ps.setString(1, request);
 			ps.setInt(2, userid);
 			ps.setString(3, requestType);
+			boolean success = ps.execute();
+			if(success) {
+				return true;
+			}
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
+		return false;
+	}
+	public boolean approveRequest(String reason,String role) {
+		String sql = "update requests set approvalstage = ?,set reason = ?;";
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			//roles are department head direct supervisor and benco
+			switch(role) {
+			case "directSupervisor":
+				ps.setString(1, "directSupervisor");
+				break;
+			case "departmentHead":
+				ps.setString(1, "departmentHead");
+				break;
+			case "BenCo":
+				ps.setString(1, "BenCo");
+				break;
+			default:
+				ps.setString(1, "none");
+				break;
+			}
+			ps.setString(2, reason);
+			boolean success = ps.execute();
+			if(success) {
+				return true;
+			}
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 }
