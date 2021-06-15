068e3099c60f3569cfe15ddda14c4def54f6d410
Tyler Throop <tyler.throop@gmail.com>
2021-06-15 07:40:58 -0700

    Adding more grade functionality so taht a user can verify the grades and then approve them

--- Project1BackEnd/src/main/java/com/revature/daos/SubmissionDAO.java
+++ Project1BackEnd/src/main/java/com/revature/daos/SubmissionDAO.java
@@ -15,11 +15,7 @@ public class SubmissionDAO implements Presentations, Grades{
 	private Connection conn = JDBCConnection.getConnection();
 	//requests has an r_id which is the user id
 	//and then an id that youll want to reference for a foreign key
-	@Override
-	public void getGrade(int id) {
-		// TODO Auto-generated method stub
-		
-	}
+	
 
 	@Override
 	public void approvePresentation(int id) {
@@ -32,17 +28,34 @@ public class SubmissionDAO implements Presentations, Grades{
 		// TODO Auto-generated method stub
 		
 	}
-
 	@Override
-	public void approveGrade(int id) {
+	public void sendPresentation(int id) {
 		// TODO Auto-generated method stub
 		
 	}
+	@Override
+	public boolean approveGrade(int id,String reason,boolean approval) {
+		String sql = "update grades set approval = ? , reason = ? where id = ?;";
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setBoolean(1, approval);
+			ps.setString(2, reason);
+			ps.setInt(3, id);
+			boolean success = ps.execute();
+			if(success) {
+				return true;
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
 
-	
+	//the grades has a reason and approval status
+	//same with the presentations
 	@Override
 	public boolean sendGrade(String grade,int approvalId) {
-		String sql = "insert into grades values(default,?,?);";
+		String sql = "insert into grades values(default,?,?,'none',false);";
 		try {
 			PreparedStatement ps = conn.prepareStatement(sql);
 			ps.setString(1, grade);
@@ -57,12 +70,22 @@ public class SubmissionDAO implements Presentations, Grades{
 		}
 		return false;
 	}
-
-	@Override
-	public void sendPresentation(int id) {
-		// TODO Auto-generated method stub
-		
+	public String getGrade(int id) {
+		String sql = "select * from grades where id = ?;";
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()) {
+				String temp = rs.getString("usergrade");
+				return temp;
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return "no grade found";
 	}
+	
 	
 
 }
--- Project1BackEnd/src/main/java/com/revature/models/Grades.java
+++ Project1BackEnd/src/main/java/com/revature/models/Grades.java
@@ -1,8 +1,8 @@
 package com.revature.models;
 
 public interface Grades {
-public void getGrade(int id);
-public void approveGrade(int id);
+
+public boolean approveGrade(int id,String reason,boolean approval);
 
 public boolean sendGrade(String grade, int approvalId);
 }
