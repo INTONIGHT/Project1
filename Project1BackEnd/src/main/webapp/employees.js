3e7d5eda402a6b8d9bba7b665ae25442af690a0e
Tyler Throop <tyler.throop@gmail.com>
2021-06-16 15:13:39 -0700

    currently the code doesnt work Im trying to do it an easier way just tough to figure out why it doesnt want to work

--- Project1BackEnd/src/main/webapp/employees.js
+++ Project1BackEnd/src/main/webapp/employees.js
@@ -5,10 +5,41 @@ xhttp.onreadystatechange = retrieveData;
 xhttp.open('GET',url);
 xhttp.send();
 function retrieveData(){
+    let dataSection = document.getElementById('data');
+    dataSection.innerHTML = '';
     if(xhttp.readyState == 4 && xhttp.status == 200){
         let response = xhttp.responseText;
         response = JSON.parse(response);
         console.log(response);
+        let employeeTable = document.createElement('table');
+    employeeTable.id = 'employeeTable';
+
+    let thRow = document.createElement('tr');
+    let tHeaders = ['id','requesttype','userrequest',
+                    'approvalstage','approvalstatus','amount',
+                'reason'    ];
+           for( let h of tHeaders){
+               let th = document.createElement('th');
+               th.innerHTML = h;
+               thRow.appendChild(th);
+           }     
+           employeeTable.append(thRow);
+           for(let employee of response){
+              let tr = document.createElement('tr');
+               for(let name of tHeaders){
+                let td = document.createElement('td');
+                let temp1 = name;
+              
+                let temp = employee.temp1;
+                td.innerHTML = temp;
+                tr.appendChild(td);
+               }
+               employeeTable.appendChild(tr);
+            //    let tdId = document.createElement('td');
+            //    tdId.innerHTML = employee.id;
+            //    tr.appendChild(tdId);
+           }
+           dataSection.appendChild(employeeTable);
     }
 }
 }
